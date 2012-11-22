0ce19f3b57273480446c8c4d043fbed9a86f5f7a
Andrei Neculau <andrei.neculau@klarna.com>
2012-11-22 11:04:20 +0100

    add github comments, css

--- assets/strap-jekyll/js/strap-jekyll-comments-github.js
+++ assets/strap-jekyll/js/strap-jekyll-comments-github.js
@@ -1,13 +1,31 @@
-function loadComments(data) {
-  for (var i=0; i<data.length; i++) {
-    var cuser = data[i].user.login;
-    var cuserlink = "https://www.github.com/" + data[i].user.login;
-    var clink = "https://github.com/izuzak/izuzak.github.com/issues/{{ page.commentIssueId }}#issuecomment-" + data[i].url.substring(data[i].url.lastIndexOf("/")+1);
-    var cbody = data[i].body_html;
-    var cavatarlink = data[i].user.avatar_url;
-    var cdate = Date.parse(data[i].created_at).toString("yyyy-MM-dd HH:mm:ss");
+function loadGithubComments(data) {
+  var i,
+      cavatar,
+      cuser,
+      cuserlink,
+      clink,
+      cdate,
+      cheader,
+      cbody,
+      url;
 
-    $("#comments").append("<div class='comment'><div class='commentheader'><div class='commentgravatar'>" + '<img src="' + cavatarlink + '" alt="" width="20" height="20">' + "</div><a class='commentuser' href=\""+ cuserlink + "\">" + cuser + "</a><a class='commentdate' href=\"" + clink + "\">" + cdate + "</a></div><div class='commentbody'>" + cbody + "</div></div>");
+  url = $('#post-comments-url').attr('href');
+
+  for (i = 0; i < data.length; i++) {
+    cavatar = data[i].user.avatar_url;
+    cuser = data[i].user.login;
+    cuserlink = 'https://www.github.com/' + data[i].user.login;
+    clink = url + '#issuecomment-' + data[i].url.substring(data[i].url.lastIndexOf('/')+1);
+    cdate = Date.parse(data[i].created_at).toString('yyyy-MM-dd HH:mm:ss');
+    cbody = data[i].body_html;
+
+    cavatar = '<div class="commentgravatar"><img src="' + cavatar + '" alt="" width="20" height="20"></div>';
+    cuser = '<a class="commentuser" href="' + cuserlink + '">' + cuser + '</a>';
+    cdate = '<a class="commentdate" href="' + clink + '">' + cdate + '</a>';
+    cheader = '<div class="commentheader">' + cavatar + cuser + cdate + '</div>'
+    cbody = '<div class="commentbody">' + cbody + '</div>'
+
+    $('#post-comments-placeholder').append('<div class="comment">' + cheader + cbody + '</div>');
   }
 }
 
